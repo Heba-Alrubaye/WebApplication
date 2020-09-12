70a91817a9a6787ef397acb596660d21e95ebe0a
Toby Stayner <staynetoby@ecs.vuw.ac.nz>
2020-09-12 14:15:18 +1200

    Prints Google sign in information

--- routes/login.js
+++ routes/login.js
@@ -1,3 +1,19 @@
-$("#loginButton").click(function(event) {
-    window.location.href = HomePage.html;
-});
+// $("#loginButton").click(function (event) {
+//     window.location.href = "/";
+// });
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    window.location.href = "/";
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
