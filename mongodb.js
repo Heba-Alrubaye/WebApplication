f3b56b6c14e41ec6eef61122f7097cb66d34382b
Heba AL-Rubaye <alrubheba@ecs.vuw.ac.nz>
2020-09-17 14:20:09 +1200

    set up mongoose connection and export user model

--- mongodb.js
+++ mongodb.js
@@ -1,4 +1,13 @@
 const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
+
+const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
+mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions);
+mongoose.Promise = global.Promise;
+
+module.exports = {
+    User: require('user.model')
+};
 
 const username = "admin";
 const password = "BiINPxSnYq4ygeRf";
