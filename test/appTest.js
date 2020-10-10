da30b90b930967ef004aeb4926d3ca52a5f73fb1
Toby Stayner <staynetoby@ecs.vuw.ac.nz>
2020-10-11 10:35:07 +1300

    Added mongodb test cases

--- test/appTest.js
+++ test/appTest.js
@@ -1,6 +1,7 @@
 const assert = require('chai').assert;
 const app = require('../app');
 
+
 describe('App', function() {
     it('dummy test', function() {
         assert(1 + 1 === 2)
---
+++ test/mongodbTest.js
@@ -0,0 +1,83 @@
+const { assert } = require('chai');
+const mongoose = require('mongoose');
+
+const username = "admin";
+const password = "BiINPxSnYq4ygeRf";
+
+const uri = `mongodb+srv://${username}:${password}@cluster0.7fsul.mongodb.net/shop`;
+
+const db = require('../mongodb');
+const User = db.User;
+
+before(function (done) {
+    mongoose.connect(uri);
+    mongoose.connection.once('open', function () {
+        console.log("Connected");
+        done();
+    }).on('error', function (error) {
+        console.log("Error", error)
+    })
+});
+
+describe('Saving records', function () {
+    it('Saves a record to the database', function (done) {
+        var user = new User({
+            email: 'vtcsuyfccwtmsswuxn@twzhhq.online',
+            hash: '$2a$10$5k62KEe2ptr2bJLnfpj4H.3BC73I7ywOISICRc1JxmLvNIyGakLNq'
+        })
+
+        user.save().then(function () {
+            assert(user.isNew === false);
+            done();
+        })
+    })
+})
+
+describe('Finding records', function () {
+    var user;
+    beforeEach(function(done) {
+        user = new User({
+            email: 'gjiovkduamqvnlumkk@miucce.online',
+            hash: '$2a$10$y7hRcUkPGokRSFlDpvK8G./o3vbkBOtxdy9CDpLpjnpQ11fvm88by'
+        })
+
+        user.save().then(function () {
+            assert(user.isNew === false);
+            done();
+        })
+    })
+
+    it('Finds one record from the database', function (done) {
+        User.findOne({ email: 'gjiovkduamqvnlumkk@miucce.online' }).then(function(result) {
+            assert(result.hash === '$2a$10$y7hRcUkPGokRSFlDpvK8G./o3vbkBOtxdy9CDpLpjnpQ11fvm88by')
+            done();
+        })
+    })
+
+    it('Finds one record by ID from the database', function (done) {
+        User.findOne({ _id: user._id }).then(function(result) {
+            assert(result._id.toString() === user._id.toString())
+            done();
+        })
+    })
+
+    afterEach(function(done) {
+        User.findOneAndRemove({ email: user.email, hash: user.hash }).then(function () {
+            User.findOne({ email: user.email }).then(function (result) {
+                assert(result === null);
+                done();
+            })
+        })
+    })
+})
+
+describe('Deleting records', function () {
+    it('Deletes one record from the database', function (done) {
+        User.findOneAndRemove({ email: 'vtcsuyfccwtmsswuxn@twzhhq.online', hash: '$2a$10$5k62KEe2ptr2bJLnfpj4H.3BC73I7ywOISICRc1JxmLvNIyGakLNq' }).then(function () {
+            User.findOne({ email: 'vtcsuyfccwtmsswuxn@twzhhq.online' }).then(function (result) {
+                assert(result === null);
+                done();
+            })
+        })
+    })
+})
